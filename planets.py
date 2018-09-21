473efb64df78b93cf0a2fa1a8b632a9903c25082
Hunter Waite <hwaite@unix2.csc.calpoly.edu>
2018-09-21 16:50:41 -0700

    planets commit

--- planets.py
+++ planets.py
@@ -1,6 +1,12 @@
 def weight_on_planets():
-   # write your code here
-   
+    weight =  float(input("What do you weigh on earth? "))
+    print("\nOn Mars you would weigh",mars_weight(weight),"pounds.")
+    print("On Jupiter you would weigh",jupiter_weight(weight),"pounds.")
+
+def mars_weight(weight):
+    return weight * .38
+def jupiter_weight(weight):
+    return weight * 2.34
    
    
 if __name__ == '__main__':
